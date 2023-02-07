bfcaaec5cca3af47fbb843a26fe70c66126020c2
composer <2789706336@qq.com>
2023-02-07 22:24:07 +0800

    feat: update jwt

---
+++ config/jwt/jwt.go
@@ -0,0 +1,54 @@
+package jwt
+
+import (
+	"context"
+	"fmt"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/hertz-contrib/jwt"
+	"github.com/joker-star-l/dousheng_common/config/log"
+	common "github.com/joker-star-l/dousheng_common/entity"
+	util_json "github.com/joker-star-l/dousheng_common/util/json"
+	"time"
+)
+
+const (
+	KeyIdentity = "identity"
+	KeyToken    = "token"
+	KeyData     = "data"
+)
+
+var Middleware *jwt.HertzJWTMiddleware
+
+func init() {
+	var err error
+	Middleware, err = jwt.New(&jwt.HertzJWTMiddleware{
+		Realm:                       "service_demo",
+		Key:                         []byte("service_demo_key"),
+		Timeout:                     time.Hour,
+		MaxRefresh:                  time.Hour * 24 * 7,
+		TokenLookup:                 fmt.Sprintf("header: %s, query: %s, param: %s, form: %s", KeyToken, KeyToken, KeyToken, KeyToken),
+		WithoutDefaultTokenHeadName: true,
+		// 向 token 中添加信息
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			return jwt.MapClaims{KeyData: data}
+		},
+		// 解析 token
+		IdentityHandler: func(ctx context.Context, c *app.RequestContext) interface{} {
+			claims := jwt.ExtractClaims(ctx, c)
+			return claims[KeyData]
+		},
+		// 认证成功 权限校验
+		Authorizator: func(data interface{}, ctx context.Context, c *app.RequestContext) bool {
+			log.Slog.Infof("auth success: %v", util_json.Str(data))
+			return true
+		},
+		// 认证失败
+		Unauthorized: func(ctx context.Context, c *app.RequestContext, code int, message string) {
+			log.Slog.Errorf("auth error: %v", message)
+			c.JSON(code, common.ErrorResponse("认证失败"))
+		},
+	})
+	if err != nil {
+		log.Slog.Panicf("HertzJWTMiddleware init error: %v", err.Error())
+	}
+}
--- util/hertz/hertz.go
+++ util/hertz/hertz.go
@@ -2,23 +2,15 @@ package util_hertz
 
 import (
 	"context"
-	"fmt"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/middlewares/server/recovery"
 	"github.com/cloudwego/hertz/pkg/app/server"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 	"github.com/hertz-contrib/cors"
-	"github.com/hertz-contrib/jwt"
 	"github.com/joker-star-l/dousheng_common/config/log"
 	common "github.com/joker-star-l/dousheng_common/entity"
 	"strconv"
 	"time"
-)
-
-const (
-	KeyIdentity = "identity"
-	KeyToken    = "token"
-	KeyData     = "data"
 )
 
 func InitServer(port int) *server.Hertz {
@@ -49,39 +41,4 @@ func UseCors(h *server.Hertz, allowOrigins []string) {
 		}
 		h.Use(cors.New(config))
 	}
-}
-
-func DefaultJwtMiddleware() *jwt.HertzJWTMiddleware {
-	jwtMiddleware, err := jwt.New(&jwt.HertzJWTMiddleware{
-		Realm:                       "service_demo",
-		Key:                         []byte("service_demo_key"),
-		Timeout:                     time.Hour,
-		MaxRefresh:                  time.Hour * 24 * 7,
-		TokenLookup:                 fmt.Sprintf("header: %s, query: %s, param: %s, form: %s", KeyToken, KeyToken, KeyToken, KeyToken),
-		WithoutDefaultTokenHeadName: true,
-		// 登录认证
-		//		Authenticator: nil,
-		// 登录时向 token 中添加字段
-		PayloadFunc: func(data interface{}) jwt.MapClaims {
-			return jwt.MapClaims{KeyData: data}
-		},
-		// 登录返回值
-		//		LoginResponse: nil
-		// 解析 token
-		IdentityHandler: func(ctx context.Context, c *app.RequestContext) interface{} {
-			claims := jwt.ExtractClaims(ctx, c)
-			return claims[KeyData]
-		},
-		// 认证成功 权限校验
-		//		Authorizator: nil,
-		// 认证失败
-		Unauthorized: func(ctx context.Context, c *app.RequestContext, code int, message string) {
-			log.Slog.Errorf("auth error: %v", message)
-			c.JSON(code, common.ErrorResponse("认证失败"))
-		},
-	})
-	if err != nil {
-		log.Slog.Panicf("HertzJWTMiddleware init error: %v", err.Error())
-	}
-	return jwtMiddleware
 }
